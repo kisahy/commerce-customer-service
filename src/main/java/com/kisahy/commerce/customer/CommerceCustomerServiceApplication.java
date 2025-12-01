75f420e47f0ce8eb2b33066fe52e07494cd71445
Gaffel <gaffel@ip-192-168-10-17.ap-northeast-2.compute.internal>
2025-12-01 16:28:47 +0900

    init project

---
+++ src/main/java/com/kisahy/commerce/customer/CommerceCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kisahy.commerce.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceCustomerServiceApplication.class, args);
+    }
+
+}
